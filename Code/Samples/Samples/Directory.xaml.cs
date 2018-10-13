d6e76e6063eaf04415558823020c5f6cb183fbe6
hexu <444194401@qq.com>
2018-10-13 17:14:33 +0800

    首次提交

---
+++ Code/Samples/Samples/Directory.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Samples
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Directory : ContentPage
+	{
+		public Directory ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Image_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Base.Image());
+        }
+    }
+}
